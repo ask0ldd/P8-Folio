51e8a2e7bf8a93a4016057fdfff07f7d25041d59
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-01-22 13:37:49 +0100

    case title

---
+++ src/components/CaseTitle.jsx
@@ -0,0 +1,9 @@
+import '../styles/CaseTitle.css'
+
+const CaseTitle = (props) => {
+return(
+    <div className="CaseTitle"><span>{props.title}</span></div>
+)
+}
+
+export default Tag
--- src/pages/Case.jsx
+++ src/pages/Case.jsx
@@ -1,18 +1,19 @@
 import '../App.css'
 import '../styles/Case.css'
 import NavBar from '../components/NavBar'
+import CaseTitle from '../components/CaseTitle'
 
 function Case(props) {
 
   return (
     <div className="case">
-        <section className='caseBody'>
+        <section className='summaryBody'>
             <NavBar items={[['ENG / FR', '#'], ['Design Work','../#works'], ['Dev Case Studies','../#caseStudies'], ['Contact Me','../#works']]} />
-            <div className='caseBlock'>
-                <div className='halfCaseBlock'>
-                    <img src="/bluel/bluel1.jpg"/>
+            <div className='summaryContainer'>
+                <img src="/bluel/bluel1.jpg" className='mainSiteCapture'/>
+                <div className='halfSummaryContainer'>
+                  <CaseTitle title="details of the assignment"/>
                 </div>
-                <div className='halfCaseBlock'></div>
             </div>
         </section>
     </div>
