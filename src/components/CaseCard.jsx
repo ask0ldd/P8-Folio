4ae8a842808c66d7566be767e64e5b75ad60792f
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-01-30 20:58:36 +0100

    casecard key bp

--- src/components/CaseCard.jsx
+++ src/components/CaseCard.jsx
@@ -7,19 +7,19 @@ import { NavLink, Link } from 'react-router-dom'
 const CaseCard = ({caseInfos}) => {
 return(
 <div className="caseContainer">
-    <button className='viewCaseButton' title="decorative element Bluel"><img alt="arrow" src={arrow}/></button>
-    <Link aria-label='Look at Sophie Bluel case study' to={caseInfos.url}><button className='viewCaseButtonGlass' title="go to case 1"></button></Link>
+    <button className='viewCaseButton' title="decorative button"><img alt="arrow" src={arrow}/></button>
+    <Link aria-label='Link to the full case study' to={caseInfos.url}><button className='viewCaseButtonGlass' title='go to case study'></button></Link>
     <span className='digit'>{caseInfos.digit}</span>
     <span className='caseTitle'>{caseInfos.title}</span>
     <ul>
-        {caseInfos.bulletpoints.map(bp => <li>{bp}</li>)}
+        {caseInfos.bulletpoints.map((bp, index) => <li key={'bp'+index+caseInfos.digit}>{bp}</li>)}
     </ul>
     <div className='tagContainer'>
         <Tag text={caseInfos.tags[0]} />
         <Tag text={caseInfos.tags[1]} />
         <Tag text={caseInfos.tags[2]} />
     </div>
-    <Link aria-label='Look at Sophie Bluel case study' to={caseInfos.url}><div className='textViewCaseButton'>Check out this project</div></Link>
+    <Link aria-label='Link to the full case study' to={caseInfos.url}><div className='textViewCaseButton'>Check out this project</div></Link>
 </div>)}
 
 export default CaseCard
