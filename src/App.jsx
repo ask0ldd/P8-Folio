5196fe44a7ebae2fb2e812acd215af1580c12bb9
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-01-31 22:25:16 +0100

    txts updated

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,8 @@ function App() {
         <div className='heroContainer'>
           <p className='fonction'>UI Design & Front End Dev. Paris. France.</p>
           <p className='identite'>Cédric ASkldd</p>
-          <p className='text'>Après avoir multiplié les prestations modestes ces dernières années (design, front-end, full stack, 3D), j'ai décidé, en 2022, de mettre un terme à ma carrière
-en tant que musicien professionnel, de manière à mieux me focaliser sur développement. Je poursuis actuellement un cursus dans l'espoir d'obtenir, à terme, le titre d'ingénieur / concepteur logiciel.</p>
+          <p className='text'>Après avoir multiplié les prestations modestes ces dernières années - design, front-end, full stack, 3D -, j'ai décidé, en 2022, de mettre un terme à ma carrière
+de musicien professionnel, de manière à mieux me focaliser sur développement. Je poursuis actuellement un cursus dans l'espoir d'obtenir, à terme, le titre d'ingénieur / concepteur logiciel.</p>
           <a href="#works"><button className='recentWork'>Recent Work</button></a>
           <blockquote>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue velit integer viverra dignissim proin.” ― Arthur Schopenhauer</blockquote>
         </div>
