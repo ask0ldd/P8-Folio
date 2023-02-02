6c17823166605be164d80a787677f0f99ede54f1
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-02-02 17:36:39 +0100

    bug menu mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ return(
     <nav>
         <ul>
             {props.mode==='fullmenu' && props.items.map(item => <li className='textMenuItems' key={item[0]}><a href={item[1]}>{item[0]}</a></li>)}
-            {props.mode==='backonly' && <li><Link to={'..'}><div><img className='backArrow' alt="back" src={backArrow}/>BACK</div></Link></li>}
+            {props.mode==='backonly' && <li className='forceArrow'><Link to={'..'}><div><img className='backArrow' alt="back" src={backArrow}/>BACK</div></Link></li>}
             {props.mode==='fullmenu' && <li className='menuItem'><img src={menuIcon} alt="menu"/></li>}
         </ul>
     </nav>
