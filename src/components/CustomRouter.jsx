346017afb09562089a8154263d17ec7746fa210f
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-02-02 18:29:33 +0100

    comments out

--- src/components/CustomRouter.jsx
+++ src/components/CustomRouter.jsx
@@ -1,8 +1,5 @@
 import Home from '../App';
 import Case from '../pages/Case';
-/*import Page404 from '../pages/Page404';
-import CaseStudy from '../pages/CaseStudy';
-import Rental from '../pages/Rental';*/
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const CustomRouter = () => {
@@ -12,12 +9,6 @@ const CustomRouter = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/case/:id" element={<Case />}/>
                 <Route path='*' element={<Home />} />
-                {/*
-                <Route path='*' element={<Page404 />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/apropos" element={<APropos />}/>
-                <Route path="/case/:id" element={<CaseStudy />}/>
-                */}
             </Routes>
         </Router>
     )
